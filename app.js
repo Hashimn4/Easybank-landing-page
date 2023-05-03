9481d08fece30713b710364e668c604ccaf615e8
Hashim Nadeem <hashimnadeem04@gmail.com>
2023-05-03 14:54:16 -0400

    Styled the tablet layout, mainly focusing on the navbar

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 let burger = document.querySelector(".header__burger");
-let close = document.querySelector(".nav__close");
+let closeBtn = document.querySelector(".nav__close");
 let menu = document.querySelector(".header__nav-secondary");
 
+burger.addEventListener("click", () => {
+    burger.classList.toggle("disappear");
+    menu.classList.toggle("active");
+    closeBtn.classList.toggle("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    closeBtn.classList.toggle("active");
+    burger.classList.toggle("disappear");
+})
